2bc6b47dbd39741e1b107d8b817d8d069986f59a
FabienFattore <aiolos79xbox@gmail.com>
2023-05-25 10:19:55 +0200

    retrait graph

--- index.js
+++ index.js
@@ -23,10 +23,6 @@ function filtrerPokemon() {
     })
 }
 
-function filtrerPokemonType() {
-    
-}
-
 fetch('https://pokeapi.co/api/v2/pokemon?limit=150')
     .then(response => response.json())
     .then(data => {
